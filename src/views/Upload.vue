dc815dbcc807941c90acc6ee93922f2a8e9470ff
MazzCode <mazzcode@gmail.com>
2022-12-27 22:50:18 -0300

    progress bar

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -2,7 +2,16 @@
   <div v-if="respuesta">
     <div class="pt-3 w-full flex justify-center items-center">
       <h1 class="text-blue-600 font-bold">Importar HVI</h1>
-      <button @click="logout()">DATOS USUARIO</button>
+      <!-- <button @click="logout()">DATOS USUARIO</button> -->
+    </div>
+    <div v-if="isLoading" class=" mx-auto h-18 w-96">
+      <p class="text-blue-600 font-bold m-auto text-center">CARGANDO A LA BASE DE DATOS, ESTE PROCESO PODRIA TOMAR UNOS MINUTOS</p>
+      <div class="mx-auto w-32">
+        <svg aria-hidden="true" class="w-20 h-20 mx-auto mt-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+         </svg>
+      </div>
     </div>
     <div
       class="
@@ -122,8 +131,20 @@
       </table>
     </div>
   </div>
-  <div class="min-h-screen w-full bg-blue-600 text-white font-extrabold flex justify-center items-center" v-else>
-    <p class=" text-4xl">
+  <div
+    class="
+      min-h-screen
+      w-full
+      bg-blue-600
+      text-white
+      font-extrabold
+      flex
+      justify-center
+      items-center
+    "
+    v-else
+  >
+    <p class="text-4xl">
       PARA TENER ACCESO A LA APLICACION DEBES INICIAR SESION
     </p>
   </div>
@@ -132,7 +153,7 @@
 <script>
 import axios from "axios";
 import router from "@/router/index.js";
-import {onMounted, ref} from 'vue'
+import { onMounted, ref } from "vue";
 
 export default {
   data: () => ({
@@ -140,7 +161,8 @@ export default {
     FileName: "",
     datos: false,
     recap: false,
-    rendering: false   
+    rendering: false,
+    isLoading: false,
   }),
 
   methods: {
@@ -193,23 +215,28 @@ export default {
       }
     },
 
-    HVIRecap() {
+   async HVIRecap() {
       let self = this;
       let jsons = {
         HVIList: self.items,
         Title: self.FileName,
       };
-
-      axios.post("http://localhost:8000/api/HVI", jsons).then((res) => {
+      this.isLoading = true;
+    try {
+     await axios.post("http://localhost:8000/api/HVI", jsons).then((res) => {
         console.log(res);
         if (res.status === 200) alert("Se enviaron los datos correctamente.");
-        this.HVIList = []
-        router.push("/recap")
-      });
-    }, 
+        this.HVIList = [];
+        // router.push("/recap");
+      });      
+    } catch (error) {
+      console.error(error)
+    }      
+    this.isLoading = false;
+    },
 
-    // datosUsuario(){      
-    // let url = 'http://localhost:8000/api/User/user' 
+    // datosUsuario(){
+    // let url = 'http://localhost:8000/api/User/user'
     // let config = {
     // headers:{'Content-Type': 'application/json'},
     // withCredentials: true
@@ -218,8 +245,7 @@ export default {
     //  console.log(this.probando);
     //  this.respuesta = true
     // }
-   
-  },   
+  },
 
   computed: {
     isTableEmpty() {
@@ -227,44 +253,40 @@ export default {
     },
   },
 
-  setup(){
-
+  setup() {
     let respuesta = ref(false);
-    onMounted( () =>{  
-    let url = 'http://localhost:8000/api/User/user' 
-    let config = {
-     headers:{'Content-Type': 'application/json'},
-     withCredentials: true
-    }
-     axios.get(url, config).then(res =>{
-      console.log(res);  
-      if(res.status === 200){
-        respuesta.value = true   
-      }    
-         
-     })     
-    })
+    onMounted(() => {
+      let url = "http://localhost:8000/api/User/user";
+      let config = {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      };
+      axios.get(url, config).then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          respuesta.value = true;
+        }
+      });
+    });
 
-    const logout = async () =>{
-    let url = 'http://localhost:8000/api/User/logout' 
-    let config = {
-     headers:{'Content-Type': 'application/json'},
-     withCredentials: true
-    }     
-   axios.post(url, config).then(res=>{
-    console.log(res);
-    // if(res.status === 200){
-    //   router.push("/")
-    // }
-   })
-  }
+    const logout = async () => {
+      let url = "http://localhost:8000/api/User/logout";
+      let config = {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      };
+      axios.post(url, config).then((res) => {
+        console.log(res);
+        // if(res.status === 200){
+        //   router.push("/")
+        // }
+      });
+    };
 
-    return{
+    return {
       respuesta,
-      logout
-    } 
-  }
-
- 
+      logout,
+    };
+  },
 };
 </script>
